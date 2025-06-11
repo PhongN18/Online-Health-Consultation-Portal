6e16f1bbfc908b6babdc53ab079df1303a9f173e
trungpham291 <trungpham29103.com>
2025-06-11 15:22:44 +0700

    VideoCall

--- BackendOHCP/Migrations/20250611080743_AddVideoSession.Designer.cs
+++ BackendOHCP/Migrations/20250611080743_AddVideoSession.Designer.cs
@@ -4,6 +4,7 @@ using BackendOHCP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackendOHCP.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250611080743_AddVideoSession")]
+    partial class AddVideoSession
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
